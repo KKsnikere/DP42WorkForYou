a3a47e2cdc5797912f72d073c7a6eb8a4a70c868
marek <marek@rvt.lv>
2024-10-01 10:24:50 +0300

    home page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../src/assets/index.css'
+import axios from 'axios'
 
 const app = createApp(App)
 
