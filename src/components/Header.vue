bee53436601b0b8ae568d2298f6cf4fc9990cb78
21DP2KSnik <106649035+KKsnikere@users.noreply.github.com>
2024-10-14 12:16:34 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -47,11 +47,11 @@
       </template>
       <template v-else>
         <li class="flex items-center gap-3 text-gray-500">
-          <router-link to="/favourites" class="py-2 px-4 flex items-center gap-3 hover:text-green-500 cursor-pointer">
+          <router-link to="/favourites" class="py-2 px-4 flex items-center gap-3 hover:text-greener cursor-pointer">
             <img src="../assets/Images/heart.svg" alt="Favorite" width="25" height="25" />
             <p class="text-base">Favourites</p>
           </router-link>
-          <router-link to="/profile" class="flex items-center gap-3 hover:text-green-500 cursor-pointer mr-5">
+          <router-link to="/profile" class="flex items-center gap-3 hover:text-greener cursor-pointer mr-5">
             <img src="../assets/Images/Profile.png" alt="Profile" width="30" height="30" />
             <p class="text-sm">Profile</p>
           </router-link>
